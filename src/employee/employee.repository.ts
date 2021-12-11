9905622223cd45b939d1d6b2557638200808bfaf
Rajnish Aarya <rajnish.aarya@codinova.com>
2021-12-11 17:02:03 +0530

    fix import error

--- src/employee/employee.repository.ts
+++ src/employee/employee.repository.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { BaseRepository } from 'src/shared/repository/base.repository';
-import { UserDocument } from 'src/user/schema/user.schema';
-import { CreateEmployeeRequestWithManagerDto } from './dtos/request/create-employee-request.dto';
 import { Employee, EmployeeDocument } from './schema/employee.schema';
 
 @Injectable()
